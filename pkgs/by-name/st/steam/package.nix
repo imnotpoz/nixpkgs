5e793bd166fc6b67d6adf390c4e895a6ddf90357
poz <poz@poz.pet>
2025-10-10 19:08:23 +0200

    remove networkmanager from steam deps

--- pkgs/by-name/st/steam/package.nix
+++ pkgs/by-name/st/steam/package.nix
@@ -85,7 +85,8 @@ let
             libva
             vulkan-loader
 
-            networkmanager
+            # doesn't build
+            # networkmanager
             # not documented, used for network status things in Big Picture
             # FIXME: figure out how to only build libnm?
             libcap # not documented, required by srt-bwrap
