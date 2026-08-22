8afae4120004e01240604363ffb99f8f6e6f0ca6
poz <poz@poz.pet>
2026-08-22 17:29:41 +0200

    nixos/vellum: fix missing attribute on empty settings

--- nixos/modules/programs/vellum.nix
+++ nixos/modules/programs/vellum.nix
@@ -67,7 +67,9 @@ in
       after = [ "graphical-session.target" ];
       partOf = [ "graphical-session.target" ];
       wantedBy = [ "graphical-session.target" ];
-      restartTriggers = [ config.environment.etc."xdg/vellum/config.toml".source ];
+      restartTriggers = mkIf (cfg.settings != { }) [
+        config.environment.etc."xdg/vellum/config.toml".source
+      ];
       serviceConfig = {
         ExecStart = "${getExe cfg.package} ${cfg.extraOptions}";
         Restart = "on-failure";
